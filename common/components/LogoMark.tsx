75d1abd7f2a9a461745699d4cf20699c6261f634
seb <sebastian.1546@gmail.com>
2026-05-18 17:29:40 +1000

    eslint and style

--- common/components/LogoMark.tsx
+++ common/components/LogoMark.tsx
@@ -1,12 +1,6 @@
 export function LogoMark({ size = 14 }: { size?: number }) {
   return (
     // eslint-disable-next-line @next/next/no-img-element
-    <img
-      src="/logo.svg"
-      alt="groundwork logo"
-      width={size}
-      height={size}
-      aria-hidden="true"
-    />
+    <img src="/logo.svg" alt="groundwork logo" width={size} height={size} aria-hidden="true" />
   )
 }
